2581430ede95f4e93b667aca54e10ab15d65725e
Judy Chen <time.fly.arrow@gmail.com>
2015-05-14 00:54:37 -0400

    fixed redirect link, set temporary user for testing

--- js/bg.js
+++ js/bg.js
@@ -1,6 +1,11 @@
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
+	var x = "profile.php?id=100009427066396";
+    if(tab.url.toLowerCase().indexOf("facebook.com") > -1){
         chrome.pageAction.show(tab.id);
 
+        chrome.storage.sync.get(x, function(data){
+            if(tab.url.toLowerCase().indexOf("facebook.com/" + x) > -1){
+                chrome.tabs.update(tab.id, {url: "http://thenicestplaceontheinter.net"});
             }
         });
     }
