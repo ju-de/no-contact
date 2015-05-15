7c765f4383a7972c30ffdaeda22f5c319aee1bd7
Judy Chen <time.fly.arrow@gmail.com>
2015-05-14 23:26:58 -0400

    to kill a feed item

---
+++ js/no.js
@@ -0,0 +1,15 @@
+function sayonara(item){
+	item.style.display = "none";
+}
+function(){
+	chrome.storage.sync.get(function(data){
+		_.each(storyContainerClasses, function(storyContainerClass){
+			posts = document.getElementsByClassName(storyContainerClass);
+			
+			//g'bye!
+			_.each(posts, function(post){
+				sayonara(post);
+			});
+		});
+	});
+}
