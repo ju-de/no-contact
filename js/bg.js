7e1714e32f888d1fc17f0c951bf5eee1291172b2
Judy Chen <time.fly.arrow@gmail.com>
2015-05-14 22:01:09 -0400

    go button revamp

--- js/bg.js
+++ js/bg.js
@@ -1,10 +1,10 @@
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
-	var x = "reeves.schraner";
+	var url = "reeves.schraner";
     if(tab.url.toLowerCase().indexOf("facebook.com") > -1){
         chrome.pageAction.show(tab.id);
 
         chrome.storage.sync.get(x, function(data){
-            if(tab.url.toLowerCase().indexOf("facebook.com/" + x) > -1){
+            if(tab.url.toLowerCase().indexOf(url) > -1){
                 chrome.tabs.update(tab.id, {url: "http://thenicestplaceontheinter.net"});
             }
         });
