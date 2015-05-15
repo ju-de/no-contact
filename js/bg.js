f0bd2dc15e2c10050bccffe43e2d410afaf3cebc
Judy Chen <time.fly.arrow@gmail.com>
2015-05-14 23:43:07 -0400

    cleanup

--- js/bg.js
+++ js/bg.js
@@ -1,10 +1,11 @@
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
-	var url = "reeves.schraner";
+	var x = "100009427066396";
     if(tab.url.toLowerCase().indexOf("facebook.com") > -1){
         chrome.pageAction.show(tab.id);
 
+        //redirect that shit
         chrome.storage.sync.get(x, function(data){
-            if(tab.url.toLowerCase().indexOf(url) > -1){
+            if(tab.url.toLowerCase().indexOf(x) > -1){
                 chrome.tabs.update(tab.id, {url: "http://thenicestplaceontheinter.net"});
             }
         });
--- js/no.js
+++ js/no.js
@@ -5,7 +5,7 @@ function(){
 	chrome.storage.sync.get(function(data){
 		_.each(storyContainerClasses, function(storyContainerClass){
 			posts = document.getElementsByClassName(storyContainerClass);
-			
+
 			//g'bye!
 			_.each(posts, function(post){
 				sayonara(post);
