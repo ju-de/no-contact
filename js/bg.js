cf46eb284830280319bfdf7bd3991b584b994a2a
Judy Chen <time.fly.arrow@gmail.com>
2015-05-27 20:19:31 -0400

    no-fb

--- js/bg.js
+++ js/bg.js
@@ -1,7 +1,7 @@
 /* this one redirects */
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
 
-	var x = "adskfjlad";
+	var x = "";
 
     if(tab.url.toLowerCase().indexOf("facebook.com") > -1){
         chrome.pageAction.show(tab.id);
@@ -9,7 +9,7 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab){
         //redirect that shit
         chrome.storage.sync.get(x, function(data){
             if(tab.url.toLowerCase().indexOf(x) > -1){
-                chrome.tabs.update(tab.id, {url: "http://thenicestplaceontheinter.net"});
+                chrome.tabs.update(tab.id, {url: "https://www.messenger.com"});
             }
         });
     }
